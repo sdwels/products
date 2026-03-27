db82c4733a2f59fe57ba187e11edc47ea8ee5c43
sdwels <wnsdd25@gmail.com>
2026-03-28 04:36:04 +0800

    Update db.php

--- db.php
+++ db.php
@@ -9,3 +9,5 @@ if (!$conn) {
     die("Connection Failed:  ". mysqli_connect_error());
 }
 ?>
+
+<!-- product_id (int 11), product_name (varchar 225), product_image (longblob), price (int 11), stocks (int 11) -->
