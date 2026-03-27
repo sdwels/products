6b35d8eaa96a853c9157ec4ec7c71726391e462f
sdwels <wnsdd25@gmail.com>
2026-03-28 04:26:57 +0800

    Add showimage.php to display product images

---
+++ showimage.php
@@ -0,0 +1,21 @@
+<?php
+include 'db.php';
+
+if (!isset($_GET['id'])) die("No ID specified.");
+
+$id = intval($_GET['id']);
+
+$sql = "SELECT product_image FROM products WHERE product_id = $id";
+$result = $conn->query($sql);
+
+if ($result->num_rows == 0) die ("No image found.");
+
+$row = $result->fetch_assoc();
+
+$finfo = finfo_open();
+$type = finfo_buffer($finfo, $row['product_image'], FILEINFO_MIME_TYPE);
+finfo_close($finfo);
+
+header ("Content-Type: $type");
+echo $row['product_image'];
+?>
